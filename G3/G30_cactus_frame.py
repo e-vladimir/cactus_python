fba38ff3650c1eddcbdb42bb67576df5293dd3c0
e_vladimir <79600069487@yandex.ru>
2024-07-26 20:30:58 +0300

    * Фиксация ошибок

--- G3/G30_cactus_frame.py
+++ G3/G30_cactus_frame.py
@@ -205,20 +205,20 @@ class C30_StructFrame(C20_MetaFrame):
 			result.code = CODES_COMPLETION.INTERRUPTED
 			result.subcodes.add(CODES_DATA.NO_DATA)
 
-		elif container.Type_RAM().flag       :
+		elif container.Type_RAM().data       :
 			result.subcodes.add(CODES_PROCESSING.SKIP)
 
 		elif not CheckIdc(cls._idc):
 			result.code = CODES_COMPLETION.INTERRUPTED
 			result.subcodes.add(CODES_DATA.ERROR_CHECK)
 
-		elif container.Type_SQLite().flag    :
+		elif container.Type_SQLite().data    :
 			result_register = container.RegisterClass(cls._idc)
 
 			result.code     = result_register.code
 			result.subcodes = result_register.subcodes
 
-		elif container.Type_PostgreSQL().flag:
+		elif container.Type_PostgreSQL().data:
 			result_register = container.RegisterClass(cls._idc)
 
 			result.code     = result_register.code
